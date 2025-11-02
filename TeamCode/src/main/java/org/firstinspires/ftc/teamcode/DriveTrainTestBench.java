22b527ab94b9992eb3a19e4e21073000b0c56e69
jan-raghavan <verbose1983@gmail.com>
2025-11-02 18:56:52 -0500

    DriveTrain TestBench

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTestBench.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.Spinner;
+
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@TeleOp(name = "DriveTrainTestBench")
+
+public class DriveTrainTestBench extends NextFTCOpMode {
+
+    public DriveTrainTestBench() {
+        addComponents(
+                new SubsystemComponent(DriveTrain.getInstance()),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    @Override
+    public void onInit() {
+        //Set up the colorSensor
+        DriveTrain.getInstance().initialize();
+    }
+
+    @Override
+    public void onUpdate() {
+
+        telemetry.update();
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        DriveTrain.getInstance().startDrive.schedule();
+    }
+}
