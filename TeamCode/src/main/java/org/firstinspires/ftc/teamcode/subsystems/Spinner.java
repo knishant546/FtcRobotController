865d8275aa8ea541660e445fcb3f1262b8a85d39
siddd514 <siddhumk2010@gmail.com>
2025-11-18 07:51:50 -0500

    Move Color Sensor Detection from Main to Spinner Subsystem . To avoid duplicate code in all

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spinner.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+
+import org.firstinspires.ftc.teamcode.Utils;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.MotorEx;
 
 import dev.nextftc.hardware.powerable.SetPower;
@@ -9,12 +17,15 @@ import dev.nextftc.hardware.powerable.SetPower;
 public class Spinner implements Subsystem {
 
     private static final Spinner INSTANCE = new Spinner();
+    private AtomicBoolean running =  new AtomicBoolean(false);
     public static Spinner getInstance() {
         return INSTANCE;
     }
 
+    private NormalizedColorSensor colorSensor;
+
     //TODO will change this name to pickMotor
-    private MotorEx spinnerMotor = new MotorEx("spinner");
+    private final MotorEx spinnerMotor = new MotorEx("spinner");
 
     private double pow = -0.65;
 
@@ -28,6 +39,36 @@ public class Spinner implements Subsystem {
     public void initialize() {
         this.stopSpinner().schedule();
         setPower(-0.65);
+    }
+
+    public void setColorSensor(NormalizedColorSensor colorSensor){
+        this.colorSensor = colorSensor;
+    }
+
+    @Override
+    public void periodic() {
+        if (colorSensor != null) {
+            controlBasedColor();
+        }
+    }
+
+    /**
+     * Check the Color Sensor and if it detects the color , then stop the spinner
+     * else start the spinner
+     */
+    private void controlBasedColor(){
+        float[] rgba = Utils.getRGBA(colorSensor);
+        String color = Utils.detectColorName(rgba);
+        ActiveOpMode.telemetry().addData("Color Detected :",color);
+        if(color.equals("Nothing")) {
+          //  if (running.compareAndSet(false,true)) {
+                startSpinner().schedule();
+         //   }
+        }else{
+        //    if (running.compareAndSet(true,false)) {
+                stopSpinner().schedule();
+        //    }
+        }
     }
 
     public void setPower(double pow) {
